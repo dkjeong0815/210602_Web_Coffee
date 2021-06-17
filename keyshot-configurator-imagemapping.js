c0a18607084aeca0ac17b6168eef913b04f5734c
dkjeong0815 <dkjeong0815@gmail.com>
2021-06-17 11:43:45 +0900

    Coffee Machine-Interactive Web
    Coffee Machine-Interactive Web Configurator by Keyshot 10

---
+++ keyshot-configurator-imagemapping.js
@@ -0,0 +1,93 @@
+imagemappingdata =
+[
+    {
+        "image": "Web Coffee.26.1.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "14+3"
+        ]
+    },
+    {
+        "image": "Web Coffee.26.2.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "14+2"
+        ]
+    },
+    {
+        "image": "Web Coffee.26.3.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "14+5"
+        ]
+    },
+    {
+        "image": "Web Coffee.26.4.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "14+10"
+        ]
+    },
+    {
+        "image": "Web Coffee.26.5.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "14+6"
+        ]
+    },
+    {
+        "image": "Web Coffee.26.6.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "14+11"
+        ]
+    },
+    {
+        "image": "Web Coffee.26.7.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "14+7"
+        ]
+    },
+    {
+        "image": "Web Coffee.26.8.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "14+12"
+        ]
+    },
+    {
+        "image": "Web Coffee.26.9.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "14+4"
+        ]
+    },
+    {
+        "image": "Web Coffee.26.10.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "14+9"
+        ]
+    }
+];
---
+++ keyshot-configurator-modeldata.js
@@ -0,0 +1,93 @@
+model =
+{
+    "materialgroups": [
+        {
+            "materials": [
+                {
+                    "icon": "%ea%b2%80.png",
+                    "multimaterials": [
+                        "14+3"
+                    ],
+                    "name": "검"
+                },
+                {
+                    "checked": true,
+                    "icon": "%eb%b9%9b%ea%b2%80.png",
+                    "multimaterials": [
+                        "14+2"
+                    ],
+                    "name": "빛검"
+                },
+                {
+                    "icon": "%ec%b4%88.png",
+                    "multimaterials": [
+                        "14+5"
+                    ],
+                    "name": "초"
+                },
+                {
+                    "icon": "%eb%b9%9b%ec%b4%88.png",
+                    "multimaterials": [
+                        "14+10"
+                    ],
+                    "name": "빛초"
+                },
+                {
+                    "icon": "%ed%9a%8c.png",
+                    "multimaterials": [
+                        "14+6"
+                    ],
+                    "name": "회"
+                },
+                {
+                    "icon": "%eb%b9%9b%ed%9a%8c.png",
+                    "multimaterials": [
+                        "14+11"
+                    ],
+                    "name": "빛회"
+                },
+                {
+                    "icon": "%eb%b9%a8.png",
+                    "multimaterials": [
+                        "14+7"
+                    ],
+                    "name": "빨"
+                },
+                {
+                    "icon": "%eb%b9%9b%eb%b9%a8.png",
+                    "multimaterials": [
+                        "14+12"
+                    ],
+                    "name": "빛빨"
+                },
+                {
+                    "icon": "%ed%8c%8c.png",
+                    "multimaterials": [
+                        "14+4"
+                    ],
+                    "name": "파"
+                },
+                {
+                    "icon": "%eb%b9%9b%ed%8c%8c.png",
+                    "multimaterials": [
+                        "14+9"
+                    ],
+                    "name": "빛파"
+                }
+            ],
+            "name": "Color"
+        }
+    ],
+    "products": [
+        {
+            "checked": true,
+            "icon": "default.png",
+            "materialgroups": [
+                "Color"
+            ],
+            "modelset": 1004,
+            "name": "Default"
+        }
+    ],
+    "studios": []
+};
